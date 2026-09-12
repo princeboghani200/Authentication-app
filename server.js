bae57a8afaf70256bd0adc96b8f31ae420b8e257
princeboghani200 <k90976697@gmail.com>
2026-09-13 00:01:11 +0530

    normal cookie

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const session = require("express-session");
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  session({
+    secret: "AuthenticationApp",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60000 }, //change after done
+  }),
+);
 
 const USER = {
   userId: "prince",
@@ -16,6 +26,7 @@ app.post("/login", (req, res) => {
   const { userId, password } = req.body;
 
   if (userId === USER.userId && password === USER.password) {
+    req.session.userId = userId;
     res.json({ message: "Login successful!" });
   } else {
     res.status(401).json({ message: "Invalid credentials" });
@@ -23,7 +34,11 @@ app.post("/login", (req, res) => {
 });
 
 app.get("/private", (req, res) => {
-  res.json({ message: "This is supposed to be a private API!" });
+  if (req.session.userId) {
+    res.json({ message: `Wlcome ${req.session.userId}, This is private !` });
+  } else {
+    res.status(401).json({ message: "Please login first." });
+  }
 });
 
 app.listen(3000, () => {
